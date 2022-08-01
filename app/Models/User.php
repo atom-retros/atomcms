c67eca03e1e9204c89ced820fbc4fe9dcc43f3ef
ObjectRetros <dennisobject@gmail.com>
2022-08-01 23:31:52 +0200

    Remove wrongly imported code

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
-use function Symfony\Component\Translation\t;
 
 class User extends Authenticatable
 {
