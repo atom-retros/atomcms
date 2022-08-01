2bc56a90e6f17b26db77fc9d0cbfe57916f55c73
ObjectRetros <dennisobject@gmail.com>
2022-08-01 23:39:31 +0200

    Refactor currency boxes into component

--- resources/themes/noname/views/home.blade.php
+++ resources/themes/noname/views/home.blade.php
@@ -11,33 +11,37 @@
         </div>
 
         <div class="grid grid-cols-4 gap-3">
-            <div class="flex h-[45px]">
-                <div class="w-1/3 bg-yellow-400 rounded-l-lg flex items-center justify-center">
+            <x-currency-box primary-color="bg-yellow-300" secondary-color="bg-yellow-400">
+                <x-slot:icon>
                     <img src="https://habstar.net/assets/images/icons/credits.png" alt="credits" class="w-5 drop-shadow">
-                </div>
-                <div class="p-2 rounded-lg rounded-l-none bg-yellow-300 w-2/3 font-bold flex justify-center items-center">{{ $user->credits }}</div>
-            </div>
+                </x-slot:icon>
 
-            <div class="flex h-[45px]">
-                <div class="w-1/3 bg-purple-400 rounded-l-lg flex items-center justify-center">
+                {{ $user->credits }}
+            </x-currency-box>
+            
+            <x-currency-box primary-color="bg-purple-300" secondary-color="bg-purple-400">
+                <x-slot:icon>
                     <img src="https://habstar.net/assets/images/icons/duckets.png" alt="duckets" class="w-5 drop-shadow">
-                </div>
-                <div class="p-2 rounded-lg rounded-l-none bg-purple-300 w-2/3 font-bold flex justify-center items-center">{{ $user->currency('duckets') }}</div>
-            </div>
+                </x-slot:icon>
 
-            <div class="flex h-[45px]">
-                <div class="w-1/3 bg-blue-400 rounded-l-lg flex items-center justify-center">
+                {{ $user->currency('duckets') }}
+            </x-currency-box>
+
+            <x-currency-box primary-color="bg-blue-300" secondary-color="bg-blue-400">
+                <x-slot:icon>
                     <img src="https://habstar.net/assets/images/icons/diamond.png" alt="Diamonds" class="w-5 drop-shadow">
-                </div>
-                <div class="p-2 rounded-lg rounded-l-none bg-blue-300 w-2/3 font-bold flex justify-center items-center">{{ $user->currency('diamonds') }}</div>
-            </div>
+                </x-slot:icon>
 
-            <div class="flex h-[45px]">
-                <div class="w-1/3 bg-gray-600 rounded-l-lg flex items-center justify-center">
+                {{ $user->currency('diamonds') }}
+            </x-currency-box>
+
+            <x-currency-box primary-color="bg-gray-400" secondary-color="bg-gray-600">
+                <x-slot:icon>
                     <img src="https://habstar.net/assets/images/icons/rank.png" alt="Rank" class="w-5 drop-shadow">
-                </div>
-                <div class="p-2 rounded-lg rounded-l-none bg-gray-400 w-2/3 font-bold flex justify-center items-center">{{ $user->permission->rank_name }}</div>
-            </div>
+                </x-slot:icon>
+
+                {{ $user->permission->rank_name }}
+            </x-currency-box>
         </div>
     </div>
 
