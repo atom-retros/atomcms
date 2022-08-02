b41d6a4f7b34bbfa3a53541a36f57e9639f8ffbe
ObjectRetros <dennisobject@gmail.com>
2022-08-02 21:38:03 +0200

    Only eager load specific columns for rank

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
     public function index()
     {
         return view('home', [
-            'user' => Auth::user()->load('permission'),
+            'user' => Auth::user()->load('permission:id,rank_name'),
         ]);
     }
 }
