52dfdfa05aefc25c1a81a4c69b70b238dca27d58
ObjectRetros <dennisobject@gmail.com>
2022-08-04 01:45:34 +0200

    Minor footer correction

--- resources/themes/noname/views/layouts/app.blade.php
+++ resources/themes/noname/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
             <div class="font-bold">&copy {{ date('Y') }} - {{ __(':hotel is a not for profit educational project', ['hotel' => setting('hotel_name')]) }}</div>
             <div></div>
             <div class="flex gap-x-1">
-                {{ __('All rights reserved. Made with') }}
+                Made with
 
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-red-700 animate-pulse" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
