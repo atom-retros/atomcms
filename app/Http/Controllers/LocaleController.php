b6bd375911ffdb50748bea965805a49c438f3956
ObjectRetros <dennisobject@gmail.com>
2022-08-04 17:32:07 +0200

    Implement language setter

---
+++ app/Http/Controllers/LocaleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WebsiteLanguage;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class LocaleController extends Controller
+{
+    public function __invoke($locale)
+    {
+        $languages = WebsiteLanguage::query()
+            ->select('country_code')
+            ->get()
+            ->pluck('country_code')
+            ->toArray();
+
+        if (! in_array($locale, $languages)) {
+            return redirect()->back()->withErrors(['message' => __('The language selected is not supported')]);
+        }
+
+        App::setLocale($locale);
+        Session::put('locale', $locale);
+
+        return redirect()->back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LocaleController;
 use App\Http\Controllers\MeController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 })->name('welcome');
+
+Route::get('/language/{locale}', LocaleController::class)->name('language.select');
 
 
 Route::middleware('auth')->group(function () {
