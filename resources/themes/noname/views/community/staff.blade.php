1507d5214f0213f591c812867c34a71ca0b831f3
ObjectRetros <dennisobject@gmail.com>
2022-08-06 08:24:12 +0200

    More spacing between staff ranks and card

--- resources/themes/noname/views/community/staff.blade.php
+++ resources/themes/noname/views/community/staff.blade.php
@@ -2,11 +2,11 @@
     <div class="col-span-12">
         <div class="flex flex-col gap-y-4">
             @foreach($employees as $employee)
-                <h2 class="font-bold text-xl border-b">
+                <h2 class="font-bold text-xl border-b mt-2">
                     {{ $employee->rank_name }}
                 </h2>
 
-                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     @foreach($employee->users as $staff)
                         <x-community.staff-card :user="$staff"/>
                     @endforeach
