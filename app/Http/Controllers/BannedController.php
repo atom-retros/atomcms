4f44258f8e041b746ecbace9e105907a76aef8e0
ObjectRetros <dennisobject@gmail.com>
2022-08-15 22:11:52 +0200

    :sparkles: Add ban system

---
+++ app/Http/Controllers/BannedController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ban;
+use Illuminate\Support\Facades\Auth;
+
+class BannedController extends Controller
+{
+    public function __invoke()
+    {
+        $ipBan = Ban::query()
+            ->where('ip', '=', request()->ip())
+            ->where('ban_expire', '>', time())
+            ->orderByDesc('id')
+            ->first();
+
+        return view('banned', [
+            'ban' => $ipBan ?? Auth::user()->ban
+        ]);
+    }
+}
