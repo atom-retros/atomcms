585034f9390f48709b2c4d184cfa573e3ebd0c29
Live <41640774+LiveZX@users.noreply.github.com>
2022-08-15 22:26:31 +0200

    Init portuguese language

--- database/seeders/WebsiteLanguageSeeder.php
+++ database/seeders/WebsiteLanguageSeeder.php
@@ -15,6 +15,7 @@ class WebsiteLanguageSeeder extends Seeder
             ['country_code' => 'fi', 'language' => 'Finnish'],
             ['country_code' => 'de', 'language' => 'German'],
             ['country_code' => 'fr', 'language' => 'French'],
+            ['country_code' => 'pt', 'language' => 'Portuguese'],
         ];
 
         WebsiteLanguage::query()->upsert($languages, ['country_code', 'language']);
