4556a7a70b3372119029239e56f5e0d79df8c9c0
benycodes <bentrigg123@gmail.com>
2022-08-17 17:53:53 +0100

    Added helper to fix Cloudflare IPs
    Makes it so that the correct IP is used in request()->ip() when Cloudflare is being used.

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -2,6 +2,10 @@
 
 use App\Models\WebsiteSetting;
 
+if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+    $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+}
+
 function setting(string $setting): string
 {
     return WebsiteSetting::query()->where('key', '=', $setting)->first()->value ?? '';
