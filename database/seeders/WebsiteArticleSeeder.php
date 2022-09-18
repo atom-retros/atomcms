cd03dc160daa6750674b558fb3b0f0c90059dc05
ObjectRetros <dennisobject@gmail.com>
2022-09-18 15:01:25 +0200

    :lipstick: Update article layout

--- database/seeders/WebsiteArticleSeeder.php
+++ database/seeders/WebsiteArticleSeeder.php
@@ -15,16 +15,16 @@ class WebsiteArticleSeeder extends Seeder
             User::factory()->create();
         }
 
-        $title = sprintf('Welcome to %s', setting('hotel_name'));
-        WebsiteArticle::query()->upsert([
-            [
-                'slug' => Str::slug($title),
-                'title' => $title,
-                'short_story' => 'Welcome to your new hotel!',
-                'full_story' => sprintf('<strong>Welcome to your new hotel</strong>!<br><br>First of all thank you for using No Name CMS - it truly means a lot❤️<br><br>We built No Name CMS to give you and your users the best possible experience when it comes to visiting %s and we hope the journey has been a pleasure for you so far.<br><br>We built No Name CMS around modern and industry approved technologies in order to give you the most robust CMS possible.<br><br>No Name CMS was made around the idea of accessibility and for you to be able to customise it without being a PHP expert or frontend guru. If you are yet to notice, the CMS comes with its <strong>own theme system</strong>! If you wish to build your own theme, then head over to the <a style="color: #42b0f5; text-decoration: underline;" href="https://github.com/ObjectRetros/atomcms" target="__blank">GitHub</a> and have a look at the readme. It gives you a good idea on how to achieve what you wish!<br><br>With everyting being said it is time to wrap up the introduction but before we do that we want to <strong>wish you the best of luck with %s</strong>!', setting('hotel_name'), setting('hotel_name')),
-                'user_id' => User::first()->id,
-                'image' => 'https://i.imgur.com/uGLDOUu.png',
-            ]
-        ], ['slug']);
+        $title = 'Atom CMS has been installed';
+        $slug = Str::slug($title);
+
+        WebsiteArticle::query()->firstOrCreate(['slug' => $slug], [
+            'slug' => $slug,
+            'title' => $title,
+            'short_story' => 'Welcome to your new hotel, we are super happy that you chose to use Atom CMS!',
+            'full_story' => sprintf('<strong>Welcome to your new hotel</strong>!<br><br>First of all thank you for using Atom CMS - it truly means a lot❤️<br><br>We built Atom CMS for you and your users to get the best possible experience when it comes to visiting %s, and we hope the journey has been a pleasure for you so far.<br><br>We have used modern and industry approved technologies (Laravel & Tailwind CSS) in order to give you the most secure & robust CMS possible.<br><br>Our idea of a good CMS is <strong>accessibility</strong> and just that, is what we have tried to make Atom CMS, so that you can be able to customise it without being a PHP expert or frontend guru. <br/><br/><strong>Some of the built in features</strong><br/>Atom CMS comes packed with tons of features, we will however only mention a few that might help to improve your hotel further!<br/><ul><li>- <a style="color: #42b0f5; text-decoration: underline;" href="https://retros.guide/docs/atom-cms/vpn-block" target="_blank">VPN / IP manager</a> - Allows you to whitelist & blacklist specific IPs or ASNs</li><li>- <a style="color: #42b0f5; text-decoration: underline;" href="https://retros.guide/docs/atom-cms/themes" target="_blank">Theme system</a> - Switch between themes easily or built your own with the simplicity of running a single command!</li><li>- <a style="color: #42b0f5; text-decoration: underline;" href="https://retros.guide/docs/atom-cms/recaptcha" target="_blank">Google ReCaptcha</a> - Keep bots away from your site, by simply enabling Googles recaptcha</li><li>- Built in <a style="color: #42b0f5; text-decoration: underline;" href="https://retros.guide/docs/atom-cms/language" target="_blank">multi language support</a> - Allow your users to browse your site in their preferred language</li></ul><br/><strong>Built in theme system!</strong><br/> Atom CMS has its own theme system, making it an absolute breeze to brew up a new theme or switch themes between the existing ones. If you wish to build your own theme, but is a bit unsure how to start, then head over to the <a style="color: #42b0f5; text-decoration: underline;" href="https://retros.guide/docs/atom-cms/themes" target="__blank">Our documentation site</a>. It gives you a good idea on how to use the theming system and tons of other aspects of running your hotel✨<br><br>With everything being said it is time to wrap up the introduction, but before we do that we want to <strong>wish you the best of luck with your hotel</strong>!', setting('hotel_name'), setting('hotel_name')),
+            'user_id' => User::first()->id,
+            'image' => 'https://i.imgur.com/uGLDOUu.png',
+        ]);
     }
 }
