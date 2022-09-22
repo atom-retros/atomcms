995121676ad32df6c84eb20187b923a98f5d45cb
ObjectRetros <dennisobject@gmail.com>
2022-09-22 20:03:52 +0200

    :lipstick: Update footer

--- resources/themes/atom/views/components/footer.blade.php
+++ resources/themes/atom/views/components/footer.blade.php
@@ -1,13 +1,17 @@
-<footer class="w-full h-14 bg-gray-100 mt-auto flex flex-col justify-center md:flex-row md:justify-between text-gray-400 items-center md:px-8 text-sm">
-    <div class="md:font-semibold text-[12px] md:text-[14px]">
-        &copy {{ date('Y') }} - {{ __(':hotel is a not for profit educational project', ['hotel' => setting('hotel_name')]) }}
+<footer
+    class="w-full h-14 bg-gray-100 mt-auto flex flex-col justify-center md:flex-row md:justify-center text-gray-400 items-center md:px-8 text-sm" onclick="showFooter()">
+    <div class="md:font-semibold text-[12px] md:text-[14px] cursor-pointer hover:underline">
+        &copy {{ date('Y') }} -
+        {{ __(':hotel is a not for profit educational project', ['hotel' => setting('hotel_name')]) }}
     </div>
-    <div class="flex gap-x-1">
-        {{ __('Atom CMS. Made with') }}
-
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-red-700 animate-pulse" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
-        </svg>
-
-        {{ __('By') }} <a href="https://devbest.com/members/object.78351/" target="_blank" class="font-semibold underline transition ease-in-out duration-150 hover:scale-105">Object</a></div>
 </footer>
+
+<script>
+    function showFooter() {
+        Swal.fire(
+            '<span class="text-[26px]">{{ setting('hotel_name') }}</span>',
+            '<span class="text-sm">{{ __('Thank you for playing :hotel. We have put a lot of effort into making the hotel what it is, and we truly appreciate you being here❤️', ['hotel' => setting('hotel_name')]) }}</span><br/><br/>  <span class="text-sm">{{ __(':hotel is driven by Atom CMS made by:', ['hotel' => setting('hotel_name')]) }} <a class="text-blue-400 underline" href="https://devbest.com/threads/atom-cms-a-multi-theme-cms.93034/" target="_blank">Object</a></span>',
+            'question'
+        )
+    }
+</script>
