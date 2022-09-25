8f49d8a371124e2dc14cbe6c75d4ce4a1a0bacbb
nicollassilva <lyod.hp@gmail.com>
2022-09-25 16:51:23 -0300

    User profile changes

--- resources/themes/atom/views/components/user/profile-info-card.blade.php
+++ resources/themes/atom/views/components/user/profile-info-card.blade.php
@@ -4,11 +4,11 @@
     {{ $image }}
 
     <div class="shadow">
-        <div class="flex gap-x-2 border-b p-3 bg-gray-50 rounded-t">
-            <p class="text-black font-semibold">{{ $title }}</p>
+        <div class="flex gap-x-2 border-b p-3 bg-gray-50 rounded-t dark:bg-gray-900">
+            <p class="text-black font-semibold dark:text-white">{{ $title }}</p>
         </div>
 
-        <section class="p-3 bg-white rounded-b">
+        <section class="p-3 bg-white rounded-b dark:bg-gray-800 dark:text-white">
             {{ $slot }}
         </section>
     </div>
