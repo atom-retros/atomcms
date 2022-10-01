74e91cd348fead59a777768a9518f764c3bac300
nicollassilva <lyod.hp@gmail.com>
2022-10-01 18:57:30 -0300

    Adding home navbar dropdown

--- resources/themes/atom/views/components/navigation/navigation-menu.blade.php
+++ resources/themes/atom/views/components/navigation/navigation-menu.blade.php
@@ -1,12 +1,36 @@
 <div class="hidden relative w-full h-full flex flex-col items-center gap-y-2 py-3 md:flex md:flex-row md:gap-x-8 md:gap-y-0 md:py-0" id="mobile-menu">
-        <a href="{{ auth()->check() ? route('me.show') : route('welcome') }}"
-           class="nav-item dark:text-gray-200 {{ request()->is('user*') ? 'md:border-b-4 md:border-b-[#eeb425]' : '' }}">
+        @if (auth()->check())
+        <button
+            id="homeDropdown"
+            data-dropdown-toggle="home-dropdown"
+            class="dark:text-gray-200 {{ request()->is('user*') || request()->is('profile*') ? 'md:border-b-4 md:border-b-[#eeb425]' : '' }} nav-item gap-x-1 ml-5 md:ml-0">
             <i class="navigation-icon home mr-1 hidden lg:inline-flex"></i>
+            {{ auth()->user()->username }}
+
+                <x-icons.chevron-down />
+        </button>
+
+        <div id="home-dropdown" class="py-2 hidden z-10 w-44 text-sm bg-white dark:bg-gray-800 shadow block">
+            <a href="{{ auth()->check() ? route('me.show') : route('welcome') }}" class="dropdown-item dark:text-gray-200 dark:hover:bg-gray-700">
                 {{ __('Home') }}
+            </a>
+
+            @auth
+            <a href="{{ route('profile.show', auth()->user()->username) }}" class="dropdown-item dark:text-gray-200 dark:hover:bg-gray-700">
+                {{ __('My Profile') }}
+            </a>
+            @endauth
+        </div>
+        @else
+        <a href="{{ route('welcome') }}"
+           class="nav-item dark:text-gray-200 {{ request()->routeIs('welcome') ? 'md:border-b-4 md:border-b-[#eeb425]' : '' }}">
+           <i class="navigation-icon home mr-1 hidden lg:inline-flex"></i>
+            {{ __('Home') }}
         </a>
+        @endif
 
         <button
-                id="dropdownNavbarLink"
+                id="communityDropdown"
                 data-dropdown-toggle="community-dropdown"
                 class="dark:text-gray-200 {{ request()->is('community*') ? 'md:border-b-4 md:border-b-[#eeb425]' : '' }} nav-item gap-x-1 ml-5 md:ml-0">
                 <i class="navigation-icon community mr-1 hidden lg:inline-flex"></i>
