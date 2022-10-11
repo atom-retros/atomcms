79442928550a764f9ffb35929b4254c9ea9f1d02
Mikkel Friis <mikkelbfriis@hotmail.com>
2022-10-11 18:21:22 +0200

    🔥 Refactor SettingsService to use laravels container to inject class instances into memory (#97)
    🔥 Refactor SettingsService to use laravels container to inject class instances into memory

--- app/Services/SettingsService.php
+++ app/Services/SettingsService.php
@@ -8,20 +8,10 @@ use Illuminate\Support\Collection;
 class SettingsService
 {
     public ?Collection $settings;
-    public static SettingsService $instance;
 
     public function __construct()
     {
         $this->settings = WebsiteSetting::all()->pluck('value', 'key');
-    }
-
-    public static function getInstance(): SettingsService
-    {
-        if (!isset(self::$instance) || !(self::$instance instanceof SettingsService)) {
-            self::$instance = new SettingsService();
-        }
-
-        return self::$instance;
     }
 
     public function getOrDefault(string $settingName, string $default = ''): ?string
