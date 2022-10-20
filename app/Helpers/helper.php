6aed7c7f523eea3f680e679244dfb4f3f7d0a27d
ObjectRetros <dennisobject@gmail.com>
2022-10-20 22:49:36 +0200

    :bug: Fix issue where every user would have same IP if hotel was using custom proxy

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -7,6 +7,11 @@ if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
     $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
 }
 
+if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+{
+    $_SERVER["REMOTE_ADDR"] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+
 if (!function_exists('setting')) {
     function setting(string $setting): string
     {
