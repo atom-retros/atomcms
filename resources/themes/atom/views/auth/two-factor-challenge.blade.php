77caf3dec14615a32c5f00f1a41b003508271627
ObjectRetros <dennisobject@gmail.com>
2022-10-30 02:40:26 +0100

    Add toggle to show recovery code input field

--- resources/themes/atom/views/auth/two-factor-challenge.blade.php
+++ resources/themes/atom/views/auth/two-factor-challenge.blade.php
@@ -2,21 +2,32 @@
     @push('title', __('Two factor challenge'))
 
     <div class="col-span-12 lg:px-[250px] flex flex-col gap-y-3">
-        <div class="rounded bg-white shadow dark:bg-gray-900 p-4">
-                <p>
-                    {{ __('Please enter your 2-factor authentication code provided on your by the authentication app on your mobile phone.') }}
-                </p>
+        <x-content.content-section icon="hotel-icon" classes="border dark:border-gray-900">
+            <x-slot:title>
+                {{ __('Two-factor verification') }}
+            </x-slot:title>
 
-                <form action="/two-factor-challenge" method="POST" class="mt-8">
+            <x-slot:under-title>
+                {{ __('Enter your 2-factor authentication code provided on your by the authentication app on your mobile phone.') }}
+            </x-slot:under-title>
+
+                <form action="/two-factor-challenge" method="POST">
                     @csrf
 
-                    <x-form.label for="code">
-                        {{ __('Code') }}
-                    </x-form.label>
+                    <div id="twoFactorCode">
+                        <x-form.label for="code">
+                            {{ __('Code') }}
 
-                    <x-form.input name="code" placeholder="{{ __('Code') }}" :autofocus="true" />
+                            <x-slot:info>
+                                {{ __('Enter the code from your authentication app') }}
+                            </x-slot:info>
+                        </x-form.label>
 
-                    <div class="mt-4">
+                        <x-form.input name="code" placeholder="{{ __('Code') }}" :autofocus="true" />
+                        <small onclick="toggleRecoveryCodeField()" class="italic text-gray-600 hover:underline hover:cursor-pointer">Lost access to your 2FA codes? Click here to use a recovery code</small>
+                    </div>
+
+                    <div id="recoveryCode">
                         <x-form.label for="recovery_code">
                             {{ __('Recovery code') }}
 
@@ -26,6 +37,7 @@
                         </x-form.label>
 
                         <x-form.input name="recovery_code" :required="false" placeholder="{{ __('Recovery code') }}" />
+                        <small onclick="toggleRecoveryCodeField()" class="italic text-gray-600 hover:underline hover:cursor-pointer">{{ __('Regained access to your 2FA codes? Click here to use your authentication app ') }}</small>
                     </div>
 
                     <div class="flex justify-end">
@@ -34,6 +46,29 @@
                         </x-form.secondary-button>
                     </div>
                 </form>
-        </div>
+        </x-content.content-section>
     </div>
+
+    @push('javascript')
+        <script>
+            document.querySelector('#recoveryCode').style.display = 'none';
+            let showRecoveryCodeField = false;
+
+            function toggleRecoveryCodeField() {
+                if(!showRecoveryCodeField) {
+                    document.querySelector('#twoFactorCode').style.display = 'none';
+                    document.querySelector('#recoveryCode').style.display = 'block';
+
+                    showRecoveryCodeField = true;
+
+                    return;
+                }
+
+                document.querySelector('#twoFactorCode').style.display = 'block';
+                document.querySelector('#recoveryCode').style.display = 'none';
+
+                showRecoveryCodeField = false;
+            }
+        </script>
+    @endpush
 </x-app-layout>
