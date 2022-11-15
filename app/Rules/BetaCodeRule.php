da8700dff59ede04d911bbab5b62b5eecd4567ac
ObjectRetros <dennisobject@gmail.com>
2022-11-15 19:22:46 +0100

    Make validation message translateable

--- app/Rules/BetaCodeRule.php
+++ app/Rules/BetaCodeRule.php
@@ -10,7 +10,7 @@ class BetaCodeRule implements InvokableRule
     public function __invoke($attribute, $value, $fail)
     {
         if (setting('requires_beta_code') && (WebsiteBetaCode::where('code', '=', $value)->doesntExist() || WebsiteBetaCode::where('code', '=', $value)->where('user_id', '!=', null)->exists())) {
-            $fail('The beta code is invalid.');
+            $fail(__('The beta code is invalid.'));
         }
     }
 }
