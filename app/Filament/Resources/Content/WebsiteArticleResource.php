48a912ab796e7831c80845ff7a1ba488511f6f0a
ObjectRetros <dennisobject@gmail.com>
2022-11-22 00:52:53 +0100

    Undo change in article resource

--- app/Filament/Resources/Content/WebsiteArticleResource.php
+++ app/Filament/Resources/Content/WebsiteArticleResource.php
@@ -29,7 +29,7 @@ class WebsiteArticleResource extends Resource
 
     protected static ?string $pluralLabel = 'articles';
 
-    protected static ?int $navigationSort = 2;
+    protected static ?int $navigationSort = 1;
 
     public static function form(Form $form): Form
     {
