26b09c4401d24c737fd41b19fbfe8427735f542c
Nícollas Silva <lyod.hp@gmail.com>
2022-11-21 20:57:11 -0300

    :sparkles: Article management (#117)

--- app/Filament/Resources/Content/WebsiteArticleResource/Pages/CreateWebsiteArticle.php
+++ app/Filament/Resources/Content/WebsiteArticleResource/Pages/CreateWebsiteArticle.php
@@ -2,11 +2,19 @@
 
 namespace App\Filament\Resources\Content\WebsiteArticleResource\Pages;
 
-use App\Filament\Resources\Content\WebsiteArticleResource;
 use Filament\Pages\Actions;
+use Illuminate\Database\Eloquent\Model;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\Content\WebsiteArticleResource;
 
 class CreateWebsiteArticle extends CreateRecord
 {
     protected static string $resource = WebsiteArticleResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $data['user_id'] = \Auth::id();
+
+        return static::getModel()::create($data);
+    }
 }
