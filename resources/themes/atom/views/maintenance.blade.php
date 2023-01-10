10664580029ecbeee828b8fe3887bcfc224ebf3a
ObjectRetros <dennisobject@gmail.com>
2023-01-10 22:37:28 +0100

    :sparkles: Move guest check

--- resources/themes/atom/views/maintenance.blade.php
+++ resources/themes/atom/views/maintenance.blade.php
@@ -15,26 +15,25 @@
 <body class="h-screen overflow-hidden" style="background: url({{ asset('assets/images/maintenance/background.png') }})">
     <x-messages.flash-messages />
 
+    <x-modals.modal-wrapper classes="flex flex-col justify-center items-center h-full relative">
+        @guest
+        <div class="absolute top-6 right-6">
+            <button @click="open = !open" class="text-black bg-white bg-opacity-70 transition ease-in-out duration-200 hover:bg-opacity-100 py-2 px-4 rounded-full font-semibold">
+                {{ __('Staff login') }}
+            </button>
+        </div>
+        @endguest
 
-    @guest
-        <x-modals.modal-wrapper classes="flex flex-col justify-center items-center h-full relative">
-            <div class="absolute top-6 right-6">
-                <button @click="open = !open" class="text-black bg-white bg-opacity-70 transition ease-in-out duration-200 hover:bg-opacity-100 py-2 px-4 rounded-full font-semibold">
-                    {{ __('Staff login') }}
-                </button>
-            </div>
+        <img src="{{ asset('assets/images/maintenance/pictures.png') }}" alt="{{ setting('hotel_name') }}">
 
-            <img src="{{ asset('assets/images/maintenance/pictures.png') }}" alt="{{ setting('hotel_name') }}">
+        <div class="text-white">
+            <h1 class="text-3xl font-semibold text-center">{{ __('Maintenance') }}</h1>
+            <p class="text-center">{{ setting('maintenance_message') }}</p>
+        </div>
 
-            <div class="text-white">
-                <h1 class="text-3xl font-semibold text-center">{{ __('Maintenance') }}</h1>
-                <p class="text-center">{{ setting('maintenance_message') }}</p>
-            </div>
-
-            <x-modals.regular-modal x-model="show {{ session()->get('wrong-auth') }}">
-                <x-auth.login-form />
-            </x-modals.regular-modal>
-        </x-modals.modal-wrapper>
-    @endguest
+        <x-modals.regular-modal x-model="show {{ session()->get('wrong-auth') }}">
+            <x-auth.login-form />
+        </x-modals.regular-modal>
+    </x-modals.modal-wrapper>
 </body>
 </html>
