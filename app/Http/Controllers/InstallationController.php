6cf840a9938d71e2a789f42b29768b3fcab31b6c
ObjectRetro <dennisobject@gmail.com>
2023-04-08 00:44:46 +0200

    cleanup

--- app/Http/Controllers/InstallationController.php
+++ app/Http/Controllers/InstallationController.php
@@ -8,7 +8,6 @@ use App\Models\WebsiteSetting;
 use App\Rules\ValidateInstallationKeyRule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
--- app/Models/WebsiteInstallation.php
+++ app/Models/WebsiteInstallation.php
@@ -9,7 +9,4 @@ class WebsiteInstallation extends Model
     protected $table = 'website_installation';
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
-
-    const IN_PROGRESS = 0;
-    const COMPLETED = 1;
 }
