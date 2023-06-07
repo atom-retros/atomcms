6ec5af08358a48dd7b22345745272f011113201d
Nick-I. A <109136581+EntenKoeniq@users.noreply.github.com>
2023-06-07 17:47:48 +0200

    ✨ Added automatic language and theme selection (#167)

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\WebsiteLanguage;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
@@ -13,8 +14,22 @@ class LocalizationMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if (Session::has('locale')) {
-            App::setLocale(Session::get('locale'));
-        }
+    App::setLocale(Session::get('locale'));
+    
+    return $next($request);
+}
+
+$countryCode = strtolower(isset($_SERVER["HTTP_CF_IPCOUNTRY"]) ? $_SERVER["HTTP_CF_IPCOUNTRY"] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
+if (WebsiteLanguage::where('country_code', '=', $countryCode)->doesntExist()) {
+    $defaultLocale = env('APP_LOCALE', 'en');
+    App::setLocale($defaultLocale);
+    Session::put('locale', $defaultLocale);
+    
+    return $next($request);
+}
+
+App::setLocale($countryCode);
+Session::put('locale', $countryCode);
 
         return $next($request);
     }
