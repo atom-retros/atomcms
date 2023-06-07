a9c795f819d8ce2c377fc83c8939f590d2d7e2d8
EntenKoeniq <109136581+EntenKoeniq@users.noreply.github.com>
2023-06-07 17:04:16 +0200

    Added automatic language and theme selection
    Based on the browser settings

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\WebsiteLanguage;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
@@ -14,6 +15,17 @@ class LocalizationMiddleware
     {
         if (Session::has('locale')) {
             App::setLocale(Session::get('locale'));
+        } else {
+            $country_code = strtolower(isset($_SERVER["HTTP_CF_IPCOUNTRY"]) ? $_SERVER["HTTP_CF_IPCOUNTRY"] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
+            $language_exist = WebsiteLanguage::where('country_code', '=', $country_code)->first();
+            if ($language_exist !== null) {
+                App::setLocale($country_code);
+                Session::put('locale', $country_code);
+            } else {
+                $default_locale = env('APP_LOCALE', 'en');
+                App::setLocale($default_locale);
+                Session::put('locale', $default_locale);
+            }
         }
 
         return $next($request);
