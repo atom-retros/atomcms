e363ccbcc257a299826910592dce2fecea3676f1
EntenKoeniq <109136581+EntenKoeniq@users.noreply.github.com>
2023-06-07 16:36:02 +0200

    Added a faster way to check if the language is supported

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -17,11 +17,8 @@ class LocalizationMiddleware
             App::setLocale(Session::get('locale'));
         } else {
             $country_code = strtolower(isset($_SERVER["HTTP_CF_IPCOUNTRY"]) ? $_SERVER["HTTP_CF_IPCOUNTRY"] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
-            $languages = WebsiteLanguage::select('country_code')
-                ->get()
-                ->pluck('country_code')
-                ->toArray();
-            if (in_array($country_code, $languages) === true) {
+            $language_exist = WebsiteLanguage::where('country_code', '=', $country_code)->first();
+            if ($language_exist !== null) {
                 App::setLocale($country_code);
                 Session::put('locale', $country_code);
             } else {
