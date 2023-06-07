f5ce51322be5fd9b3be42aea8465d6e5cec92eb8
Nick-I. A <109136581+EntenKoeniq@users.noreply.github.com>
2023-06-07 23:27:47 +0200

    ✨ Added route for rooms (#168)

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\View\View;
+
+class RoomController extends Controller
+{
+    public function __invoke(Room $room): View
+    {
+        return view('room.index', [
+            'room' => $room->load('owner:id,username,look'),
+        ]);
+    }
+}
