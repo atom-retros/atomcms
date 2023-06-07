59a85efb696fc8e6aa98124927a4a9cf359ad87e
Nick-I. A <109136581+EntenKoeniq@users.noreply.github.com>
2023-06-07 17:36:00 +0200

    Update app/Http/Middleware/LocalizationMiddleware.php
    Co-authored-by: Object <87041394+ObjectRetros@users.noreply.github.com>

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -14,19 +14,22 @@ class LocalizationMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if (Session::has('locale')) {
-            App::setLocale(Session::get('locale'));
-        } else {
-            $country_code = strtolower(isset($_SERVER["HTTP_CF_IPCOUNTRY"]) ? $_SERVER["HTTP_CF_IPCOUNTRY"] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
-            $language_exist = WebsiteLanguage::where('country_code', '=', $country_code)->first();
-            if ($language_exist !== null) {
-                App::setLocale($country_code);
-                Session::put('locale', $country_code);
-            } else {
-                $default_locale = env('APP_LOCALE', 'en');
-                App::setLocale($default_locale);
-                Session::put('locale', $default_locale);
-            }
-        }
+    App::setLocale(Session::get('locale'));
+    
+    return $next($request);
+}
+
+$countryCode = strtolower(isset($_SERVER["HTTP_CF_IPCOUNTRY"]) ? $_SERVER["HTTP_CF_IPCOUNTRY"] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
+if (WebsiteLanguage::where('country_code', '=', $countryCode)->doesntExist()) {
+    $defaultLocale = env('APP_LOCALE', 'en');
+    App::setLocale($defaultLocale);
+    Session::put('locale', $defaultLocale);
+    
+    return $next($request);
+}
+
+App::setLocale($countryCode);
+Session::put('locale', $countryCode);
 
         return $next($request);
     }
