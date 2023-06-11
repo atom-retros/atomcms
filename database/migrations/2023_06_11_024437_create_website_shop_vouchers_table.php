66b30f422347b2b576d0b4470e4b9be547384c4e
ObjectRetros <dennisobject@gmail.com>
2023-06-11 03:32:56 +0200

    Update max uses

--- database/migrations/2023_06_11_024437_create_website_shop_vouchers_table.php
+++ database/migrations/2023_06_11_024437_create_website_shop_vouchers_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('website_shop_vouchers', function (Blueprint $table) {
             $table->id();
             $table->string('code')->unique();
-            $table->unsignedInteger('max_uses');
+            $table->unsignedInteger('max_uses')->default(1);
             $table->unsignedInteger('use_count')->default(0);
             $table->unsignedInteger('amount');
             $table->timestamp('expires_at')->nullable();
