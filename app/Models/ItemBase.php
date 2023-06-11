4d5c773fe11cea5cbc98fb752b8483bebcc20734
ObjectRetros <dennisobject@gmail.com>
2023-06-11 14:24:25 +0200

    Change model name to singular form for proper representation

--- app/Models/ItemBase.php
+++ app/Models/ItemBase.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ItemsBase extends Model
+class ItemBase extends Model
 {
     protected $table = 'items_base';
     protected $guarded = ['id'];
-    
+
     public function icon(): string {
         return '/client/flash/dcr/hof_furni/' . $this->item_name . '_icon.png';
     }
