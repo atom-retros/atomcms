b02aa3a386ad7b134bd76cd1c5565a56db102f63
ObjectRetros <dennisobject@gmail.com>
2023-06-11 15:59:03 +0200

    ✨ Display amount of furniture theu ser receives when hovering the item

--- resources/themes/atom/views/components/shop/packages.blade.php
+++ resources/themes/atom/views/components/shop/packages.blade.php
@@ -39,7 +39,7 @@
             <p class="font-semibdol">Furnis:</p>
             <div class="flex gap-2">
                 @foreach ($article->furniItems() as $furni)
-                    <img data-tippy-content="{{ $furni->public_name }}" class="user-badge" src="{{$furni->icon()}}" alt="{{ $furni->public_name }}" width="36" height="36">
+                    <img data-tippy-content="{{ collect(json_decode($article->furniture))->firstWhere('item_id', $furni->id)->amount }}x {{ $furni->public_name }}" class="user-badge" src="{{$furni->icon()}}" alt="{{ $furni->public_name }}" width="36" height="36">
                 @endforeach
             </div>
         </div>
