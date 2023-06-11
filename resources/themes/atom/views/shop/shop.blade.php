955d8f2bbc9ebd2258535b0a7989a20bed2754ed
ObjectRetros <dennisobject@gmail.com>
2023-06-11 03:29:52 +0200

    🔥 Remove unnecessary card due to moving terms to modal

--- resources/themes/atom/views/shop/shop.blade.php
+++ resources/themes/atom/views/shop/shop.blade.php
@@ -93,40 +93,6 @@
                 </x-form.secondary-button>
             </form>
         </x-content.content-card>
-
-
-        <x-content.content-card icon="hotel-icon" classes="border dark:border-gray-900">
-            <x-slot:title>
-                {{ __(':hotel Shop', ['hotel' => setting('hotel_name')]) }}
-            </x-slot:title>
-
-            <x-slot:under-title>
-                {{ __('Purchase :hotel items', ['hotel' => setting('hotel_name')]) }}
-            </x-slot:under-title>
-
-            <div class="space-y-4 text-[14px] dark:text-gray-300">
-                <p>
-                    {{ __('Here at :hotel Hotel we are accepting donations to keep the hotel up & running and as a thank you, you will in return receive in-game goods.', ['hotel' => setting('hotel_name')]) }}
-                </p>
-
-                <p>
-                    <span class="font-semibold">{{ __('Why are donations important?') }}</span><br>
-                    {{ __('Donations are important, as it will help to pay our monthly bills needed to keep the hotel up & running, as well as adding new and exciting features for you and others to enjoy!') }}
-                </p>
-
-                <p class="font-semibold italic">
-                    {{ __('To purchase items from the :hotel shop, please visit our Discord and contact the owner of :hotel Hotel to make your purchase', ['hotel' => setting('hotel_name')]) }}
-                </p>
-
-                <div class="mt-4">
-                    <a href="{{ setting('discord_invitation_link') }}" target="_blank">
-                        <x-form.secondary-button>
-                            {{ __('Take me to the :hotel Discord', ['hotel' => setting('hotel_name')]) }}
-                        </x-form.secondary-button>
-                    </a>
-                </div>
-            </div>
-        </x-content.content-card>
     </div>
 
     @push('javascript')
