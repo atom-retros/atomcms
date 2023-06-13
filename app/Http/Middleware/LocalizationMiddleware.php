34e3adb15fe302cec97983b11849fa595be2bb5b
EntenKoeniq <109136581+EntenKoeniq@users.noreply.github.com>
2023-06-13 20:11:19 +0200

    ✨ Update LocalizationMiddleware.php

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -14,22 +14,30 @@ class LocalizationMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if (Session::has('locale')) {
-    App::setLocale(Session::get('locale'));
-    
-    return $next($request);
-}
+            App::setLocale(Session::get('locale'));
 
-$countryCode = strtolower(isset($_SERVER["HTTP_CF_IPCOUNTRY"]) ? $_SERVER["HTTP_CF_IPCOUNTRY"] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
-if (WebsiteLanguage::where('country_code', '=', $countryCode)->doesntExist()) {
-    $defaultLocale = env('APP_LOCALE', 'en');
-    App::setLocale($defaultLocale);
-    Session::put('locale', $defaultLocale);
-    
-    return $next($request);
-}
+            return $next($request);
+        }
 
-App::setLocale($countryCode);
-Session::put('locale', $countryCode);
+        $countryCode = env('APP_LOCALE', 'en');
+        if (isset($_SERVER["HTTP_CF_IPCOUNTRY"])) {
+            $countryCode = strtolower($_SERVER["HTTP_CF_IPCOUNTRY"]);
+        } else if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            $countryCode = strtolower(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
+        }
+
+        // If the language does not exist in the database
+        if (WebsiteLanguage::where('country_code', '=', $countryCode)->doesntExist()) {
+            $defaultCountry = env('APP_LOCALE', 'en');
+            
+            App::setLocale($defaultCountry);
+            Session::put('locale', $defaultCountry);
+            
+            return $next($request);
+        }
+
+        App::setLocale($countryCode);
+        Session::put('locale', $countryCode);
 
         return $next($request);
     }
