eb031e13a2259e00f74f85064609bbaefc47f696
DennisObject <dennisobject@gmail.com>
2023-09-03 23:08:51 +0200

    Add nav link for logo generator

--- resources/themes/atom/views/components/top-header.blade.php
+++ resources/themes/atom/views/components/top-header.blade.php
@@ -25,6 +25,7 @@
         </x-top-header-currency>
     </div>
 
+<<<<<<< Updated upstream
     <div class="flex w-full justify-between md:w-auto">
         @auth
             @if(hasPermission('view_server_logs') || hasPermission('housekeeping_access'))
@@ -53,5 +54,53 @@
             @endif
         @endauth
         <x-navigation.user-dropdown />
+=======
+    <div class="flex gap-x-3">
+        @if(hasPermission('view_server_logs') || hasPermission('housekeeping_access') || hasPermission('generate_logo'))
+            <x-navigation.dropdown classes="!text-red-700 !border-none">
+                {{ __('Administration') }}
+
+                <x-slot:children>
+                    @if (hasPermission('view_server_logs'))
+                        <x-navigation.dropdown-child route="/log-viewer" :turbolink="false" target="_blank">
+                            {{ __('Error logs') }}
+                        </x-navigation.dropdown-child>
+                    @endif
+
+                    @if (hasPermission('generate_logo'))
+                        <x-navigation.dropdown-child route="{{ route('logo-generator.index') }}" :turbolink="false" target="_blank">
+                            {{ __('Logo generator') }}
+                        </x-navigation.dropdown-child>
+                    @endif
+
+                    @if (hasPermission('housekeeping_access'))
+                        <x-navigation.dropdown-child :route="setting('housekeeping_url')" :turbolink="false" target="_blank">
+                            {{ __('Housekeeping') }}
+                        </x-navigation.dropdown-child>
+                    @endif
+                </x-slot:children>
+            </x-navigation.dropdown>
+        @endif
+
+        <x-navigation.dropdown classes="!border-none">
+            <img class="h-12" src="{{ setting('avatar_imager') }}{{ auth()->user()->look }}&direction=2&headonly=1&head_direction=2&gesture=sml" alt="{{ auth()->user()->username }}">
+
+            <span class="-ml-2">{{ auth()->user()->username }}</span>
+
+            <x-slot:children>
+                <x-navigation.dropdown-child :route="route('settings.account.show')">
+                    {{ __('User settings') }}
+                </x-navigation.dropdown-child>
+
+                <button class="dropdown-item dark:text-gray-200 dark:hover:bg-gray-700 w-full text-left" @click.prevent="document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </button>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                    @csrf
+                </form>
+            </x-slot:children>
+        </x-navigation.dropdown>
+>>>>>>> Stashed changes
     </div>
 </div>
