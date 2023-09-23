6bc69cba81f4aad9222880d0d17a4885d29a0fb8
Morten Mehus <morten.mehus@gmail.com>
2023-09-23 23:16:31 +0200

    Removed conditional check on user_id exists on table
    We control the table, we know that there is no user_id on the table
    because we're adding it during this migration

--- database/migrations/2022_08_25_225959_change_user_id_on_website_articles_table.php
+++ database/migrations/2022_08_25_225959_change_user_id_on_website_articles_table.php
@@ -8,12 +8,6 @@ return new class extends Migration
 {
     public function up(): void
     {
-        dropForeignKeyIfExists('website_articles', 'user_id');
-
-        if (Schema::hasColumn('website_articles', 'user_id')) {
-            Schema::dropColumns('website_articles', 'user_id');
-        }
-
         Schema::table('website_articles', function (Blueprint $table) {
             $table->integer('user_id')->nullable()->change();
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
