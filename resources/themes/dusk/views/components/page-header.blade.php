115427abfb8952964b94ebab2237f9f41c879f5d
ObjectRetros <dennisobject@gmail.com>
2023-12-25 20:35:30 +0100

    fix: positioning of page header titles

--- resources/themes/dusk/views/components/page-header.blade.php
+++ resources/themes/dusk/views/components/page-header.blade.php
@@ -1,11 +1,11 @@
 @props(['icon' => '', 'classes' => '', 'subHeader'])
 
-<div class="w-full bg-[#21242e] p-3 rounded-lg flex gap-2 {{ $classes }}">
+<div class="w-full bg-[#21242e] p-3 rounded-lg flex gap-2 items-center {{ $classes }}">
     @if (!empty($icon))
         {{ $icon }}
     @endif
 
-   <div class="flex-col">
+   <div class="flex-col px-2">
        <div class="text-lg font-bold text-gray-100 flex items-center">
            {{ $slot }}
        </div>
