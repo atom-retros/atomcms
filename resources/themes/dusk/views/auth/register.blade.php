f481a96f7c8567eeaa71cf68f557761eb75142b7
DuckieTM <info@camwijs.nl>
2023-12-26 23:29:28 +0100

    Fix Register & dependency

--- resources/themes/dusk/views/auth/register.blade.php
+++ resources/themes/dusk/views/auth/register.blade.php
@@ -51,63 +51,6 @@
         </form>
     </div>
 
-    {{-- Articles --}}
-    <div class="col-span-12 md:col-span-6 ">
-        <!-- Slider main container -->
-        <div class="swiper h-[250px] rounded-md">
-
-            <!-- If we need pagination -->
-            <div class="swiper-pagination"></div>
-
-            <!-- If we need navigation buttons -->
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
-
-            <!-- Additional required wrapper -->
-            <div class="swiper-wrapper" style="z-index: 14;">
-                @foreach($articles as $article)
-                    <div class="swiper-slide relative article-image" style="background-image: url({{ $article->image }})">
-                        <div class="absolute h-[90px] w-full left-0 bottom-0 bg-[#171a23] bg-opacity-[95%] text-white py-2 px-4">
-                            <h2 class="text-3xl font-bold">
-                                {{ $article->title }}
-                            </h2>
-
-                            <div class="flex justify-between items-center">
-                                <div class="py-1 px-2 rounded-md bg-black/60 text-sm mt-2 flex gap-1 items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                    </svg>
-
-                                    {{ $article->user->username }}
-                                </div>
-
-                                <a href="{{ route('article.show', $article->slug) }}" class="text-sm read-more-link hover:underline">
-                                    Read more
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-        <div class="col-span-12 grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
-            @foreach($photos as $photo)
-                <a href="{{ $photo->url }}" data-fancybox="gallery" class="cursor-pointer relative transition duration-300 ease-in-out hover:scale-[102%]">
-                    <div class="photo-overlay"></div>
-                    <img class="h-[250px] w-full object-cover object-center rounded-md custom-shadow" src="{{ $photo->url }}" alt="">
-
-                    <div class="absolute right-2 bottom-2 bg-black/70 p-2 rounded-md text-white flex gap-x-2 z-[5]">
-                        <img class="self-center" src="{{ asset('/assets/images/dusk/author_camera_icon.png') }}" alt="">
-                        <small>
-                            {{ $photo->user->username }}
-                        </small>
-                    </div>
-                </a>
-            @endforeach
-        </div>
-    </div>
-
     <script>
         function debounce(func, wait) {
             let timeout;
@@ -150,6 +93,6 @@
         usernameInput.addEventListener('keyup', updateAvatar);
 
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
+    <script src="/assets/js/fancybox.umd.js"></script>
+    <link rel="stylesheet" href="/assets/css/fancybox.css" />
 </x-app-layout>
