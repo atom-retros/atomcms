609b2e2ce86c772010d9ae059774958cf803e651
ObjectRetros <dennisobject@gmail.com>
2023-12-26 14:15:00 +0100

    fix: update users current ip when visiting the client

--- app/Http/Controllers/FlashController.php
+++ app/Http/Controllers/FlashController.php
@@ -9,6 +9,10 @@ class FlashController extends Controller
 {
     public function __invoke(): View
     {
+        Auth::user()->update([
+            'ip_current' => request()->ip(),
+        ]);
+
         return view('client.flash', [
             'sso' => Auth::user()->ssoTicket(),
         ]);
--- app/Http/Controllers/NitroController.php
+++ app/Http/Controllers/NitroController.php
@@ -9,6 +9,10 @@ class NitroController extends Controller
 {
     public function __invoke(): View
     {
+        Auth::user()->update([
+            'ip_current' => request()->ip(),
+        ]);
+
         return view('client.nitro', [
             'sso' => Auth::user()->ssoTicket(),
         ]);
