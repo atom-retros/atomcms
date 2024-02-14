b34cc97cbf4c209c0d33facf712cc0e9d58aaf3d
DennisObject <dennisobject@gmail.com>
2024-02-14 17:54:09 +0100

    feat: update nitro for dusk

--- resources/themes/dusk/views/client/nitro.blade.php
+++ resources/themes/dusk/views/client/nitro.blade.php
@@ -9,98 +9,98 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
 
-    @vite(['resources/themes/dusk/css/app.scss', 'resources/themes/dusk/js/app.js'])
+    @vite(['resources/themes/atom/css/app.css', 'resources/themes/atom/js/app.js'])
 </head>
 
 <body class="overflow-hidden" id="nitro-client">
-    <div class="absolute top-4 left-4 z-10 flex gap-x-2">
-        <a data-turbolinks="false" href="{{ route('me.show') }}">
-            <x-client.client-button>
-                <x-icons.home />
-            </x-client.client-button>
-        </a>
+<div class="absolute top-4 left-4 z-10 flex gap-x-2">
+    <a data-turbolinks="false" href="{{ route('me.show') }}">
+        <x-client.client-button>
+            <x-icons.home />
+        </x-client.client-button>
+    </a>
 
-        <div onclick="reloadClient()">
-            <x-client.client-button>
-                <x-icons.reload />
-            </x-client.client-button>
-        </div>
-
-        <div onclick="toggleFullscreen()">
-            <x-client.client-button>
-                <x-icons.fullscreen />
-            </x-client.client-button>
-        </div>
-
-        <x-client.client-button classes="flex items-center justify-center gap-x-1">
-            <x-icons.user />
-
-            <span id="online-count"></span>
+    <div onclick="reloadClient()">
+        <x-client.client-button>
+            <x-icons.reload />
         </x-client.client-button>
     </div>
-    <iframe id="nitro" src="{{ sprintf('%s/index.html?sso=%s', config('habbo.client.nitro_path'), $sso) }}"
-        class="absolute top-0 left-0 m-0 h-full w-full overflow-hidden border-none p-0"></iframe>
 
-    {{-- Show disconnected message on client if the user has been disconnected --}}
-    <div id="disconnected" class="h-screen w-full">
-        <div class="absolute h-full w-full bg-black bg-opacity-50"></div>
-
-        <div class="relative flex h-full w-full flex-col items-center justify-center gap-4">
-            <h2 class="text-2xl text-white">
-                {{ __('Whoops! It seems like you have been disconnected...') }}
-            </h2>
-
-            <div class="flex gap-x-4">
-                <button
-                    class="py-2 px-4 text-white rounded bg-[#eeb425] hover:bg-[#e3aa1e] border-2 border-[#cf9d15] transition ease-in-out"
-                    onclick="reloadClient()">
-                    {{ __('Reload client') }}
-                </button>
-
-                <a href="{{ route('me.show') }}">
-                    <x-form.secondary-button>
-                        {{ __('Back to website') }}
-                    </x-form.secondary-button>
-                </a>
-            </div>
-        </div>
+    <div onclick="toggleFullscreen()">
+        <x-client.client-button>
+            <x-icons.fullscreen />
+        </x-client.client-button>
     </div>
 
-    <script>
-        function toggleFullscreen() {
-            if (document.fullscreenElement) {
-                document.exitFullscreen();
+    <x-client.client-button classes="flex items-center justify-center gap-x-1">
+        <x-icons.user />
 
-                return;
-            }
+        <span id="online-count"></span>
+    </x-client.client-button>
+</div>
+<iframe id="nitro" src="{{ sprintf('%s/index.html?sso=%s', setting('nitro_path'), $sso) }}"
+        class="absolute top-0 left-0 m-0 h-full w-full overflow-hidden border-none p-0"></iframe>
 
-            document.documentElement.requestFullscreen();
+{{-- Show disconnected message on client if the user has been disconnected --}}
+<div id="disconnected" class="h-screen w-full">
+    <div class="absolute h-full w-full bg-black bg-opacity-50"></div>
+
+    <div class="relative flex h-full w-full flex-col items-center justify-center gap-4">
+        <h2 class="text-2xl text-white">
+            {{ __('Whoops! It seems like you have been disconnected...') }}
+        </h2>
+
+        <div class="flex gap-x-4">
+            <button
+                class="py-2 px-4 text-white rounded bg-[#eeb425] hover:bg-[#e3aa1e] border-2 border-[#cf9d15] transition ease-in-out"
+                onclick="reloadClient()">
+                {{ __('Reload client') }}
+            </button>
+
+            <a href="{{ route('me.show') }}">
+                <x-form.secondary-button>
+                    {{ __('Back to website') }}
+                </x-form.secondary-button>
+            </a>
+        </div>
+    </div>
+</div>
+
+<script>
+    function toggleFullscreen() {
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+
+            return;
         }
 
-        function reloadClient() {
-            window.location.href = window.location;
+        document.documentElement.requestFullscreen();
+    }
+
+    function reloadClient() {
+        window.location.href = window.location;
+    }
+
+    window.addEventListener('DOMContentLoaded', () => {
+        function getOnlineUserCount() {
+            fetch('{{ route('api.online-count') }}')
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(response) {
+                    document.getElementById('online-count').innerHTML = response.data.onlineCount;
+                });
         }
 
-        window.addEventListener('DOMContentLoaded', () => {
-            function getOnlineUserCount() {
-                fetch('{{ route('api.online-count') }}')
-                    .then(function(response) {
-                        return response.json();
-                    })
-                    .then(function(response) {
-                        document.getElementById('online-count').innerHTML = response.data.onlineCount;
-                    });
-            }
+        getOnlineUserCount();
 
+        setInterval(function() {
             getOnlineUserCount();
+        }, 15000);
+    });
+</script>
 
-            setInterval(function() {
-                getOnlineUserCount();
-            }, 15000);
-        });
-    </script>
-
-    <script src="{{ asset('assets/js/atom.js') }}"></script>
+<script src="{{ asset('assets/js/atom.js') }}"></script>
 </body>
 
 </html>
