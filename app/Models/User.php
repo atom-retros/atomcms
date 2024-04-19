48aac048c59b9ed052cb61577a4af5491c7be99c
Dennis <dennisobject@gmail.com>
2024-04-19 13:05:45 +0200

    fix: mobile navigation on dusk

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,6 +35,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'hidden_staff' => 'boolean',
         ];
     }
 
