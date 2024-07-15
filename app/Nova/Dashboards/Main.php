cb2def78d6ff4235a5b28461e4ec703456464976
KaniRobinson <me@kanirobinson.co.uk>
2024-07-15 21:25:38 +0100

    Added Base Metric for Dashboard

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -3,6 +3,7 @@
 namespace App\Nova\Dashboards;
 
 use Laravel\Nova\Cards\Help;
+use App\Nova\Metrics\AnalyticCard;
 use Laravel\Nova\Dashboards\Main as Dashboard;
 
 class Main extends Dashboard
@@ -15,7 +16,33 @@ class Main extends Dashboard
     public function cards()
     {
         return [
-            new Help,
+            new AnalyticCard(
+                model: \Atom\Core\Models\User::class,
+                icon: 'users',
+            ),
+            new AnalyticCard(
+                model: \Atom\Core\Models\ItemBase::class,
+                title: 'Furniture',
+                icon: 'briefcase',
+            ),
+            new AnalyticCard(
+                model: \Atom\Core\Models\Ban::class,
+                icon: 'ban',
+            ),
+            new AnalyticCard(
+                model: \Atom\Core\Models\WebsiteHelpCenterTicket::class,
+                title: 'Suport Tickets',
+                icon: 'ticket',
+            ),
+            new AnalyticCard(
+                model: \Atom\Core\Models\WebsiteArticle::class,
+                title: 'News Articles',
+                icon: 'newspaper',
+            ),
+            new AnalyticCard(
+                model: \Atom\Core\Models\Room::class,
+                icon: 'home',
+            ),
         ];
     }
 }
---
+++ app/Nova/Metrics/AnalyticCard.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Nova\Metrics;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Metrics\Value;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class AnalyticCard extends Value
+{
+    /**
+     * Create a new metric instance.
+     */
+    public function __construct(public readonly string $model, string $title = null, string $icon = null)
+    {
+        $parts = explode('\\', $model);
+
+        $this->icon = $icon ?: 'chart-bar';
+        $this->name = $title ?: Nova::humanize(str()->plural(end($parts)));
+    }
+
+    /**
+     * Calculate the value of the metric.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return mixed
+     */
+    public function calculate(NovaRequest $request)
+    {
+        return $this->result(app($this->model)->count())
+            ->allowZeroResult();
+    }
+
+    /**
+     * Determine the amount of time the results of the metric should be cached.
+     *
+     * @return \DateTimeInterface|\DateInterval|float|int|null
+     */
+    public function cacheFor()
+    {
+        // return now()->addMinutes(5);
+    }
+}
