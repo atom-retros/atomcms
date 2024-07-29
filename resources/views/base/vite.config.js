d35ea459d39c85b1f37deb3d3efd2f1cc1401296
KaniRobinson <me@kanirobinson.co.uk>
2024-07-29 23:56:22 +0100

    Cleaned up the build process

--- resources/views/base/vite.config.js
+++ resources/views/base/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import buildImages from '../../js/build/images'
-import path from "path";
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from 'tailwindcss'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -10,34 +10,13 @@ export default defineConfig({
                 path.resolve(__dirname, './css/app.css'),
                 path.resolve(__dirname, './js/app.js'),
             ],
+            refresh: true,
         }),
-        {
-            name: 'sync',
-            apply: 'build',
-            configureServer: () => buildImages(),
-            buildStart: () => buildImages(),
-        },
-        {
-            name: "blade",
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith(".blade.php")) {
-                    server.ws.send({
-                        type: "full-reload",
-                        path: "*",
-                    });
-                }
-            },
-        },
     ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, './js'),
-        },
-    },
     css: {
         postcss: {
             plugins: [
-                require("tailwindcss")({
+                tailwindcss({
                     config: path.resolve(__dirname, "tailwind.config.js"),
                 }),
             ],
