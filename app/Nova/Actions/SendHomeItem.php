db2f08f647eac04a5cb51f96a9162df0b98d242d
KaniRobinson <me@kanirobinson.co.uk>
2024-08-15 22:51:48 +0100

    Added the ability to send home items

---
+++ app/Nova/Actions/SendHomeItem.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use Illuminate\Bus\Queueable;
+use Laravel\Nova\Actions\Action;
+use Illuminate\Support\Collection;
+use Atom\Core\Models\WebsiteHomeItem;
+use Laravel\Nova\Fields\ActionFields;
+use Illuminate\Queue\InteractsWithQueue;
+use Laravel\Nova\Actions\ActionResponse;
+use Outl1ne\MultiselectField\Multiselect;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class SendHomeItem extends Action
+{
+    use InteractsWithQueue, Queueable;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        foreach ($models as $model) {
+            foreach (json_decode($fields->items) as $itemId) {
+                $model->homeItems()->attach($itemId);
+            }
+        }
+        
+        return ActionResponse::message('The item has been sent to the user\'s inventory!');
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        $options = WebsiteHomeItem::with('category')
+            ->get()
+            ->map(fn ($item) => ['id' => $item->id, 'label' => $item->name ?: '-', 'group' => $item->category->name]);
+
+        return [
+            Multiselect::make('Items', 'items')
+                ->placeholder('Search and select items to send')
+                ->options($options)
+                ->required(),
+        ];
+    }
+}
--- app/Nova/User.php
+++ app/Nova/User.php
@@ -190,6 +190,8 @@ class User extends Resource
      */
     public function actions(NovaRequest $request)
     {
-        return [];
+        return [
+            new Actions\SendHomeItem,
+        ];
     }
 }
