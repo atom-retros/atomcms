d9f4db6247bbac56b31e29c9b31755dab0457f1e
KaniRobinson <me@kanirobinson.co.uk>
2024-08-27 08:53:11 +0100

    Dropped support for the Dusk theme

--- resources/themes/dusk/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import Alpine from "alpinejs";
-import Focus from "@alpinejs/focus";
-
-window.axios = axios;
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-
-Alpine.plugin(Focus);
-Alpine.start();
--- resources/themes/dusk/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@push('title', 'Welcome to the best hotel on the web!')
-
-@section('content')
-    <p>Ready to build</p>
-@endsection
