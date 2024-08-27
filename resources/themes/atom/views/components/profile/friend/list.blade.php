e160ef04af1d9e829e846c0fe32f582af28a9614
KaniRobinson <me@kanirobinson.co.uk>
2024-08-27 13:28:58 +0100

    Added a edge case where the user has been deleted but the friendship hasnt

--- resources/themes/atom/views/components/profile/friend/list.blade.php
+++ resources/themes/atom/views/components/profile/friend/list.blade.php
@@ -5,7 +5,7 @@
 
     <x-card.base title="{{ __('Friends') }}">
         <div class="grid justify-between col-span-2 gap-3 sm:grid-cols-4 md:grid-cols-8">
-            @forelse($user->friends as $friend)
+            @forelse($user->friends->whereNotNull('friend') as $friend)
                 <x-profile.friend.item :friend="$friend" />
             @empty
                 <p class="col-span-2 text-sm font-semibold text-center dark:text-white md:col-span-8 sm:col-span-4">
