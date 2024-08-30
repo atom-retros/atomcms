4a394ff177f1c2fe106492edd6a9464cee21940f
Kani Robinson <me@kanirobinson.co.uk>
2024-08-30 22:13:56 +0000

    Enabled S3

--- config/filesystems.php
+++ config/filesystems.php
@@ -91,6 +91,7 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'visibility' => 'public',
             'throw' => false,
         ],
 
