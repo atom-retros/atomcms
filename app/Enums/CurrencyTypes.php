b3f4a182f8f40e8eca5c76c10e89cbc22c5e731c
duckietm <troop@mailtjes.com>
2024-12-19 12:43:17 +0100

    :up: Fix Achievements

--- app/Enums/CurrencyTypes.php
+++ app/Enums/CurrencyTypes.php
@@ -2,9 +2,50 @@
 
 namespace App\Enums;
 
-enum CurrencyTypes : int
+enum CurrencyTypes: int
 {
-    case DUCKETS = 0;
-    case DIAMONDS = 5;
-    case POINTS = 101;
+    case Credits = -1;
+    case Duckets = 0;
+    case Diamonds = 5;
+    case Points = 101;
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function fromCurrencyName(string $currencyName): ?self
+    {
+        $currencyName = strtolower($currencyName);
+
+        $currency = match ($currencyName) {
+            'credits' => self::Credits,
+            'duckets' => self::Duckets,
+            'diamonds' => self::Diamonds,
+            'points' => self::Points,
+            default => null,
+        };
+
+        return $currency;
+    }
+
+    public function getImage(): string
+    {
+        return match ($this->value) {
+            CurrencyTypes::Credits->value => asset('assets/images/currencies/credits.gif'),
+            CurrencyTypes::Duckets->value => asset('assets/images/currencies/duckets.png'),
+            CurrencyTypes::Diamonds->value => asset('assets/images/currencies/diamonds.png'),
+            CurrencyTypes::Points->value => asset('assets/images/currencies/points.png'),
+        };
+    }
+
+    public static function toInput(): array
+    {
+        $allCurrencies = self::cases();
+
+        return array_combine(
+            array_column($allCurrencies, 'value'),
+            array_column($allCurrencies, 'name'),
+        );
+    }
 }
